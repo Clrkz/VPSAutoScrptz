839fb3aeb07e5e45d186590bf6bf27956dd6175a
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-03-31 13:24:37 +0800

    Add files via upload

---
+++ DsblChngPW.sh
@@ -0,0 +1,3 @@
+wget -O /home/vps/public_html/view/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/login.html"
+wget -O /home/vps/public_html/view/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/setting.html"
+wget -O /home/vps/public_html/controller/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/home.php"
