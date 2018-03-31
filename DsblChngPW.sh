2f1bf54b9241d50c2c1479643f591cb52e7257f2
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-03-31 13:48:40 +0800

    Update DsblChngPW.sh

--- DsblChngPW.sh
+++ DsblChngPW.sh
@@ -1,6 +1,9 @@
 cd
 
 chmod -R g+rw /home/vps/public_html 
-wget -P /home/vps/public_html/view/login.html "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/login.html"
-wget -P /home/vps/public_html/view/setting.html "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/setting.html"
-wget -P /home/vps/public_html/controller/home.php "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/home.php"
+rm /home/vps/public_html/view/setting.html
+rm /home/vps/public_html/view/login.html
+rm /home/vps/public_html/controller/home.php
+wget -P /home/vps/public_html/view/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/login.html"
+wget -P /home/vps/public_html/viewview/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/setting.html"
+wget -P /home/vps/public_html/controller/ "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/home.php"
