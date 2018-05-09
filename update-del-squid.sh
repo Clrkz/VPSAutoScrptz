ea7725667effd459d403594c690555c6668ae0fb
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-05-09 18:24:27 +0800

    Add files via upload

---
+++ update-del-squid.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear
+echo "clear" >> .bashrc
+echo 'echo -e "      # ###       ###                  /"' >> .bashrc
+echo 'echo -e "    /  /###  /     ###               #/"' >> .bashrc
+echo 'echo -e "   /  /  ###/       ##               ##"' >> .bashrc
+echo 'echo -e "  /  ##   ##        ##               ##"' >> .bashrc
+echo 'echo -e " /  ###             ##               ##"' >> .bashrc
+echo 'echo -e "##   ##             ##  ###  /###    ##  /##   ######"' >> .bashrc
+echo 'echo -e "##   ##             ##   ###/ #### / ## / ### /#######"' >> .bashrc
+echo 'echo -e "##   ##             ##    ##   ###/  ##/   / /      ##"' >> .bashrc
+echo 'echo -e "##   ##             ##    ##         ##   /         /"' >> .bashrc
+echo 'echo -e "##   ##             ##    ##         ##  /         /"' >> .bashrc
+echo 'echo -e " ##  ##             ##    ##         ## ##        ###"' >> .bashrc
+echo 'echo -e "  ## #      /       ##    ##         ######        ###"' >> .bashrc
+echo 'echo -e "   ###     /        ##    ##         ##  ###        ###"' >> .bashrc
+echo 'echo -e "    ######/         ### / ###        ##   ### /      ##"' >> .bashrc
+echo 'echo -e "      ###            ##/   ###        ##   ##/       ##"' >> .bashrc
+echo 'echo -e "                                                     /"' >> .bashrc
+echo 'echo -e "                                                    /"' >> .bashrc
+echo 'echo -e "                                                   /"' >> .bashrc
+echo 'echo -e "                                                  /"' >> .bashrc
+echo 'echo -e "welcome to the server $HOSTNAME" | lolcat' >> .bashrc
+echo 'echo -e "Script mod by Clrkz"' >> .bashrc
+echo 'echo -e "Type menu to display a list of commands"' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+
+echo " "
+echo "                  Remove Squid User       "
+echo "                      By Clrkz                  "
+echo "*****************************************************"
+echo " "
+echo " "
+echo "Please enter a user name for Squid:"
+read u
+echo " "
+clear
+
+#Add squid
+#htpasswd -b -c /etc/squid3/squid_passwd $u $p
+
+#htpasswd -D /etc/squid3/squid_passwd $u
+
+service squid3 restart
+
+clear
+
+echo " "
+echo "***************************************************"
+echo "   Squid proxy server set up has been completed."
+echo "***************************************************"
+echo " "
+echo " "
