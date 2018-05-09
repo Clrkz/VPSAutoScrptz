92b0f311dafe09d8aebe40f585606770e8a3fef5
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-05-09 16:42:53 +0800

    Add files via upload
    OCS Update

---
+++ update-ocs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Update
+
+clear
+cd /usr/bin
+wget -O menu "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-menu.sh"
+wget -O limitdb "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-userlimitdropbear.sh"
+wget -O limitssh "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-userlimitssh.sh"
+wget -O delexp "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-delexp.sh"
+chmod +x menu
+chmod +x limitdb
+chmod +x limitssh
+chmod +x delexp
+clear
+echo " "
+echo "DONE! OCS Autoscript Update By Clrkz" 
+menu
