9876a4e39b846abbc0e87372c6f7cbf3775feff7
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-05-09 16:39:48 +0800

    Add files via upload
    ocs update

---
+++ update-menu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Menu
+
+echo -e "* menu      : Displays a list of available commands"
+echo -e "* usernew   : Creating an SSH Account"
+echo -e "* trial     : Create a Trial Account"
+echo -e "* delete    : Clearing SSH and OpenVPN Account"
+echo -e "* check     : Check User Login"
+echo -e "* member    : Check Member SSH and OpenVPN"
+echo -e "* limitdb   : Limit login Dropbear"
+echo -e "* limitssh  : Limit login SSHD"
+echo -e "* delexp    : Delete Expired User"
+echo -e "* restart   : Restart Service dropbear, webmin"
+echo -e "              squid3, OpenVPN and SSH"
+echo -e "* reboot    : reboot VPS"
+echo -e "* speedtest : speedtest VPS"
+echo -e "* info      : System Information"
+echo -e "* about     : Information about auto install script"
+echo -e "* exit      : exit Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e ""
---
+++ update-userlimitdropbear.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+PARAM=$1
+
+echo -n > /tmp/pid2
+ps ax|grep dropbear > /tmp/pid
+cat /tmp/pid | grep -i 'dropbear -p' > /tmp/pids
+cat /var/log/auth.log |  grep -i "Password auth succeeded" > /tmp/sks
+perl -pi -e 's/Password auth succeeded for//g' /tmp/sks
+perl -pi -e 's/dropbear//g' /tmp/sks
+
+cat /tmp/pid | while read line;do
+set -- $line
+p=$1
+var=`cat /tmp/sks | grep -i $1`
+set -- $var
+l=$6
+if [ "$6" != '' ]
+then
+echo "$p $l" | cat - /tmp/pid2 > /tmp/temp && mv /tmp/temp /tmp/pid2
+fi
+ done
+
+case $PARAM in
+
+1)
+echo -n > /tmp/user1
+cat /tmp/pid2 | while read line;do
+set -- $line
+p=$1
+u=$2
+cat /tmp/user1 | grep -i $u > /dev/null
+if [ $? = 1 ];then
+echo $line >> /tmp/user1
+else
+kill $p
+echo "kill $p user $u" 
+fi
+done
+rm -f /tmp/pid
+rm -f /tmp/pid2
+rm -f /tmp/pids
+rm -f /tmp/sks
+rm -f /tmp/user1
+exit 0
+;;
+2)
+echo -n > /tmp/user1
+echo -n > /tmp/user2
+cat /tmp/pid2 | while read line;do
+set -- $line
+p=$1
+u=$2
+cat /tmp/user1 | grep -i $u > /dev/null
+if [ $? = 1 ];then
+echo $line >> /tmp/user1
+else
+cat /tmp/user2 | grep -i $u > /dev/null
+if [ $? = 1 ];then
+echo $line >> /tmp/user2
+else
+kill $p
+echo "kill $p user $u"
+fi
+fi
+done
+rm -f /tmp/pid
+rm -f /tmp/pid2
+rm -f /tmp/pids
+rm -f /tmp/sks
+rm -f /tmp/user1
+rm -f /tmp/user2
+exit 0
+;;
+*)
+  echo " use limitdb 1 command to limit 1 login only"
+  echo " or limitdb 2 to limit max 2 login"   
+rm -f /tmp/pid
+rm -f /tmp/pid2
+rm -f /tmp/pids
+rm -f /tmp/sks
+exit 1
+;;
+
+esac
