76321ac42aa0fe91fa79dbaabd1e1105d4abacdc
Clrkz <34614707+Clrkz@users.noreply.github.com>
2018-05-10 03:47:00 +0800

    Add files via upload

--- update-ocs.sh
+++ update-ocs.sh
@@ -35,9 +35,9 @@ echo " View Usage Data"
 echo " Menu Update"
 echo " Added Update Support"
 echo " "
-read -r -p "Are you sure? [Y/n]" response
-response=${response,,} # tolower
-if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+ read -r -p "Are you sure? [Y/n]" response
+ response=${response,,} # tolower
+ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
 sudo apt-get install nload
 cd /usr/bin
 wget -O menu "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-menu.sh"
@@ -48,18 +48,17 @@ wget -O usage "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-usage.sh
 wget -O update "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-ocs.sh"
 wget -O ban "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-ban.sh"
 wget -O unban "https://github.com/Clrkz/VPSAutoScrptz/raw/master/update-unban.sh"
-chmod +x menu
-chmod +x limitdb
-chmod +x limitssh
-chmod +x delexp
-chmod +x usage
-chmod +x update
-chmod +x ban
-chmod +x unban
+chmod +x menu && sed -i -e 's/\r$//' menu
+chmod +x limitdb && sed -i -e 's/\r$//' limitdb
+chmod +x limitssh && sed -i -e 's/\r$//' limitssh
+chmod +x delexp && sed -i -e 's/\r$//' delexp
+chmod +x usage && sed -i -e 's/\r$//' usage
+chmod +x update && sed -i -e 's/\r$//' update
+chmod +x ban && sed -i -e 's/\r$//' ban
+chmod +x unban && sed -i -e 's/\r$//' unban
 cd
 clear
 echo " "
 echo "DONE! OCS Autoscript Update By Clrkz" 
 menu
-fi
-menu
+ fi
